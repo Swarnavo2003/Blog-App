5ec866cd1073a08165192b9227d8c33f138b3136
Swarnavo2003 <swarnavo2020@gmail.com>
2025-06-22 15:21:43 +0530

    Frontend Started

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <h1 className="font-bold text-4xl">Welcome to Blog App</h1>
+    </div>
+  );
+}
+
+export default App;
