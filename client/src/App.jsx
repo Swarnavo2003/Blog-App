f52b5ae715f3533d72adc5f6a866473556ef4f2f
Swarnavo2003 <swarnavo2020@gmail.com>
2025-06-23 17:39:26 +0530

    DaisyUI added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="bg-black text-white min-h-screen">
-      <h1 className="font-bold text-4xl">Welcome to Blog App</h1>
+    <div>
+      <h1>Welcome to Blog App</h1>
     </div>
   );
 }
